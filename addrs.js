8de92a1549e030485f4433bc1824531c03a9b6b8
Kersing Huang <kersingh@gmail.com>
2017-07-15 17:29:12 -0400

    Fix bundle.js generated by browserify to be standalone library that can be referenced by keys

--- addrs.js
+++ addrs.js
@@ -1,33 +1,29 @@
 var zcore = require('bitcore-lib-zcash');
 var Mnemonic = require('bitcore-mnemonic');
 
-var password = 'insecure pw'
-var network = 'testnet'
+var password = 'insecure pw';
+var network = 'testnet';
+var tradeId = 3;
 
 'use strict';
 
-var genPrivKey = function(password, network){
-  console.log("In genprivkey")
-  var code = new Mnemonic(Mnemonic.Words.ENGLISH);
-  var hdPrivateKey = code.toHDPrivateKey(password, network);
-  // do we want to store the hdPrivateKey in localstorage?
-  return {"code": code.toString(), "privkey": hdPrivateKey.toString()}
-}
-
-var recoverPrivKey = function(code, password, network){
-  var hdPrivateKey = new Mnemonic(code).toHDPrivateKey(password, network);
-  return hdPrivateKey
-}
-
-// for per-trade public keys. Necessary?
-var tradeId = 3;
-var newPubKey = function(hdPrivateKey, tradeId){
-  var derived = hdPrivateKey.derive(tradeId)
-  var hdPublicKey = hdPrivateKey.hdPublicKey;
-  var address = derived.privateKey.toAddress();
-  return {"pubkey": hdPublicKey, "address": address}
-}
-
-exports.genPrivKey = genPrivKey;
-exports.recoverPrivKey = recoverPrivKey;
-exports.newPubKey = newPubKey;
+module.exports = {
+  genPrivKey: function(password, network){
+    //console.log("In genprivkey")
+    var code = new Mnemonic(Mnemonic.Words.ENGLISH);
+    var hdPrivateKey = code.toHDPrivateKey(password, network);
+    // do we want to store the hdPrivateKey in localstorage?
+    return {"code": code.toString(), "privkey": hdPrivateKey.toString()}
+  },
+  recoverPrivKey: function(code, password, network){
+    var hdPrivateKey = new Mnemonic(code).toHDPrivateKey(password, network);
+    return hdPrivateKey
+  },
+  // for per-trade public keys. Necessary?
+    newPubKey: function(hdPrivateKey, tradeId){
+    var derived = hdPrivateKey.derive(tradeId)
+    var hdPublicKey = hdPrivateKey.hdPublicKey;
+    var address = derived.privateKey.toAddress();
+    return {"pubkey": hdPublicKey, "address": address};
+  }
+};
