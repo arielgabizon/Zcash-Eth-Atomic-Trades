cee55ff1d473e5eca4b8b49f347c2b00aaa3a7a1
Ariel Gabizon <ariel.gabizon@gmail.com>
2017-07-15 16:43:25 -0400

    wip debugging

--- zcash.js
+++ zcash.js
@@ -2,20 +2,15 @@ var spawn = require("child_process").spawn;
 
 // takes Alice and Bob's Zcash addrs
 // Wrap the Zcash functions
-module.exports = zcashFund
+module.exports = Zcash_fund
 module.exports = Zcash_getaddr
 module.exports = Zcash_make_contract
-
-function zcashFund(){
-  var str = 'bla'
-  var process = spawn('python',["test.py", str]);
-  console.log('in fund')
-  process.stdout.on('data', function (data){
-    console.log("data", data.toString())
-  });
-}
+module.exports = Zcash_get_secret
+module.exports = Zcash_redeem
+module.exports = Zcash_refund
 
 function Zcash_getaddr(){
+  var str = 'bla'
   var process = spawn('python',["ZBXCAT/eth.py", "0"]);
   console.log('in fund')
   process.stdout.on('data', function (data){
@@ -24,7 +19,39 @@ function Zcash_getaddr(){
 }
 
 function Zcash_make_contract(){
-  var process = spawn('python',["ZBXCAT/eth.py", "1", "contract.json"]);
+  var process = spawn('python',["ZBXCAT/eth.py", "1","contract.json"]);
+  console.log('in fund')
+  process.stdout.on('data', function (data){
+    console.log("data", data.toString())
+  });
+}
+
+function Zcash_fund(){
+  var process = spawn('python',["ZBXCAT/eth.py", "2", "contract.json"]);
+  console.log("in Zcash_fund js")
+  process.stdout.on('data', function (data){
+    console.log("data", data.toString())
+  });
+}
+function Zcash_get_secret(){
+  var str = 'bla'
+  var process = spawn('python',["ZBXCAT/eth.py", "3", "contract.json"]);
+  console.log('in fund 3')
+  process.stdout.on('data', function (data){
+    console.log("data", data.toString())
+  });
+}
+
+function Zcash_redeem(){
+  var process = spawn('python',["ZBXCAT/eth.py", "4","contract.json"]);
+  console.log('in redeem js')
+  process.stdout.on('data', function (data){
+    console.log("data", data.toString())
+  });
+}
+
+function Zcash_refund(){
+  var process = spawn('python',["ZBXCAT/eth.py", "5","contract.json"]);
   console.log('in fund')
   process.stdout.on('data', function (data){
     console.log("data", data.toString())
@@ -32,6 +59,6 @@ function Zcash_make_contract(){
 }
 
 
-// zcashFund()
+Zcash_fund()
 // Zcash_getaddr()
-Zcash_make_contract()  
+// Zcash_redeem()  
