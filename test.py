d878e95adc46d0572f6794121db043ed0e38a7bb
Jay Graber <jay@z.cash>
2017-07-15 10:46:59 -0700

    Add files for zXcat in python

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import sys
+import zXcat
 
 def doThings():
     print("Doing a thing", word)
 
 if __name__ == '__main__':
-    word = sys.argv[1]
-    doThings()
+    # word = sys.argv[1]
+    # doThings()
     # sys.stdout.flush()
+    print zXcat.privkey("tmBbzUMfAB1vsAuj3UaWi4wLJMRJJyVxUzU")
